f81c547f1937d58c3075c547f3caf86ed16b04ba
Pardio11 <l20121022@morelia.tecnm.mx>
2024-05-07 19:29:16 -0600

    Lift up serverand home path

---
+++ cmd/app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &homeHandler{})
+
+	http.ListenAndServe(":8080", mux)
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("This is my home page"))
+}
